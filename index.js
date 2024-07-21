29dcc86e981ff887b995ab7dcd892e4ad61ef0c6
Emr <127445447+EmrD@users.noreply.github.com>
2024-07-21 15:24:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,14 @@ const apiKeyMiddleware = (req, res, next) => {
     res.status(403).json({ error: 'Forbidden: Invalid API Key' });
   }
 };
+
 app.use(apiKeyMiddleware);
+
 app.get('/api/data', (req, res) => {
-  const apiKey = req.get('x-api-key');
-  res.json({ message: 'Welcome to the secured API with ${apiKey}!' });
+    const apiKey = req.get('x-api-key');
+    res.json({ message: `Welcome to the secured API with ${apiKey}!` });
 });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
